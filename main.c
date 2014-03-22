8194473e35805eb581e7019b86a54189e3101f9e
Dan Mundy <drm.mundy@gmail.com>
2014-03-21 22:22:55 -0400

    Converts currentString into a pointer, so the same string is reused.

--- main.c
+++ main.c
@@ -2,26 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
-void count(char minAscii, char maxAscii, int minElement, int maxElement, int position, char* currentString) {
-  printf("\ncount(%c, %c, %d, %d, %d, %s)\n", minAscii, maxAscii, minElement, maxElement, position, currentString);
+void count(char minAscii, char maxAscii, int minElement, int maxElement, int position, char** currentString) {
+  printf("\ncount(%c, %c, %d, %d, %d, %s)\n", minAscii, maxAscii, minElement, maxElement, position, *currentString);
 
   for (char character = minAscii; character <= maxAscii; character++) {
     // increment the character at the current position
-    currentString[position] = character;
+    (*currentString)[position] = character;
     getchar();
 
     // Print the current string numerically, as a string, and its address.
     for (int j = 0; j < maxElement; j++) {
-      printf("%d\t", currentString[j]);
+      printf("%d\t", (*currentString)[j]);
     }
-    printf("%s %p", currentString, &currentString);
+    printf("%s %p", *currentString, currentString);
 
     // increase the level of recursion
     if (position < maxElement - 1) {
       count(minAscii, maxAscii, minElement, maxElement, position + 1, currentString);
     }
   }
-  printf("\ncount(%c, %c, %d, %d, %d, %s) finished", minAscii, maxAscii, minElement, maxElement, position, currentString);
+  printf("\ncount(%c, %c, %d, %d, %d, %s) finished", minAscii, maxAscii, minElement, maxElement, position, *currentString);
 
   /*
    * Issues:
@@ -37,7 +37,10 @@ int main() {
   /* char maxAscii = 126; */
   char minAscii = 65;
   char maxAscii = 68;
-  char *currentString = (char *) malloc(sizeof(char) * maxElement);
+
+  // This is the most reliable way I could think of to make a pointer to a string.
+  char *emptyString = (char *) malloc(sizeof(char) * maxElement);
+  char **currentString = &emptyString;
 
   count(minAscii, maxAscii, minElement, maxElement, 0, currentString);
 
