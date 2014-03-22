6f159d5f9002e1114517309eb025b7b41c48e85e
Dan Mundy <drm.mundy@gmail.com>
2014-03-21 23:41:20 -0400

    Uses a more reliable string allocation method (hat tip: @wyc)

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-void count(char minAscii, char maxAscii, int minElement, int maxElement, int position, char** currentString) {
+void count(char minAscii, char maxAscii, int minElement, int maxElement, int position, char* currentString) {
   for (char character = minAscii; character <= maxAscii; character++) {
-    (*currentString)[position] = character; // Increment the character at the current position
+    currentString[position] = character; // Increment the character at the current position
 
     if (position < maxElement - 1) {
       count(minAscii, maxAscii, minElement, maxElement, position + 1, currentString); // increase the level of recursion
     } else {
-      printf("%s\n", *currentString); // Print the current string
+      printf("%s\n", currentString); // Print the current string
     }
   }
 }
@@ -21,8 +21,8 @@ int main() {
   char maxAscii = 126;
 
   // This is the most reliable way I could think of to make a pointer to a string.
-  char *emptyString = (char *) malloc(sizeof(char) * maxElement);
-  char **currentString = &emptyString;
+  char emptyString[maxElement];
+  char *currentString = emptyString;
 
   for (int length = minElement + 1; length <= maxElement; length++) {
     count(minAscii, maxAscii, minElement, length, 0, currentString);
